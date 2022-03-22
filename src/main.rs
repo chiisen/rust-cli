627e85a816ce1576412e18ad59d146c3e0b26630
edwin45168899 <chiisen@protonmail.com>
2022-03-23 05:18:59 +0800

    fix: web 初始化

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
-use actix_web::{web, App, HttpServer, Responder};
+use actix_web::{get, web, App, HttpServer, Responder};
 
-fn index(info: web::Path<(u32, String)>) -> impl Responder {
-    format!("Hello {}! id:{}", info.1, info.0)
+#[get("/{id}/{name}/index.html")]
+async fn index(params: web::Path<(u32, String)>) -> impl Responder {
+    let (id, name) = params.into_inner();
+    format!("Hello {}! id:{}", name, id)
 }
 
-fn main() -> std::io::Result<()> {
-    HttpServer::new(
-        || App::new()
-            .service(web::resource("/{id}/{name}/index.html").to(index))
-        )
-        .bind("127.0.0.1:8080")?
+#[actix_web::main] // or #[tokio::main]
+async fn main() -> std::io::Result<()> {
+    
+    println!("程式啟動!");
+
+    HttpServer::new(|| App::new().service(index))
+        .bind(("127.0.0.1", 3000))?
         .run()
+        .await
 }
