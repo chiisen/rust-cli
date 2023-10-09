73c30c2b938f6f65c8d6135639575b5936decd98
chiisne <chiisen@hotmail.com>
2023-10-09 10:03:19 +0800

    feat: 檢查檔案是否存在

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 extern crate clap;
 
 use clap::{Arg, App};
+use std::path::Path;
+use std::process;
+use std::fs::File;
+use std::io::{Read};
 
 fn main() {
-    let _matches = App::new("rust-cli")
+    let matches = App::new("rust-cli")
         .version("1.0.1")
         .author("Sam")
         .about("用 Rust 打造的第一个命令行工具")
@@ -13,7 +17,17 @@ fn main() {
         )
         .get_matches();
 
-    if let Some(file) = _matches.value_of("FILE") {
-        println!("Value for file argument: {}", file);
+    if let Some(file) = matches.value_of("FILE") {
+        if Path::new(&file).exists() {
+            println!("File exist!!");
+            let mut f = File::open(file).expect("[rust-cli Error] File not found.");
+            let mut data = String::new();
+            f.read_to_string(&mut data).expect("[rust-cli Error] Unable to read the  file.");
+            println!("{}", data);
+        }
+        else {
+            eprintln!("[rust-cli Error] No such file or directory.");
+            process::exit(1);
+        }
     }
 }
